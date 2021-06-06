d5961cf80d8b9e2f97cce565cfc7a703486bd1c1
Himanshu Batra <himanshu@batra.io>
2021-06-06 18:54:36 -0400

    WiP

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import './assets/css/tailwind.css'
+import IMG from './assets/images/react.png'
+import LOGO from './assets/images/logo.svg'
 
 export default function App() {
   return (
     <div>
-      <h1 className="text-red-500 text-5xl">
+      <h1 className="text-blue-500 text-5xl">
         Hello World - React with Webpack {process.env.name}
       </h1>
+      <img src={IMG} alt="react logo" width="400" height="400" />
+      <img src={LOGO} alt="react logo" width="400" height="400" />
     </div>
   )
 }
