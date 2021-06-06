42725844276cfca664e166101eaaedc4802885ae
Himanshu Batra <himanshu@batra.io>
2021-06-06 16:58:06 -0400

    WiP

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import './assets/css/tailwind.css'
 export default function App() {
   return (
     <div>
-      <h1 className="text-blue-500 text-5xl">
+      <h1 className="text-red-500 text-5xl">
         Hello World - React with Webpack {process.env.name}
       </h1>
     </div>
