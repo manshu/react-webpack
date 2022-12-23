1a6108f49973cf267f89de2aa61286dba2ffb0f0
Himanshu Batra <himanshu@batra.io>
2022-12-23 13:48:28 -0500

    WiP

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,10 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  mode: 'jit',
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
+    content: [
+        './src/**/*.{js,jsx,ts,tsx}', './public/index.html'
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
 }
--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -6,68 +6,68 @@ const tailwindcss = require('tailwindcss')
 const autoprefixer = require('autoprefixer') // help tailwindcss to work
 
 module.exports = {
-  entry: path.resolve(__dirname, '..', './src/index.tsx'),
-  resolve: {
-    extensions: ['.tsx', '.ts', 'jsx', '.js'],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(ts|js)x?$/,
-        exclude: /node_modules/,
-        use: [
-          {
-            loader: 'babel-loader',
-          },
-        ],
-      },
-      {
-        test: /\.css$/,
-        use: [
-          {
-            loader: MiniCssExtractPlugin.loader,
-            options: {},
-          },
-          {
-            loader: 'css-loader',
-            options: {
-              importLoaders: 1,
+    entry: path.resolve(__dirname, '..', './src/index.jsx'),
+    resolve: {
+        extensions: ['.tsx', '.ts', '.jsx', '.js'],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(ts|js)x?$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: 'babel-loader',
+                    },
+                ],
             },
-          },
-          {
-            loader: 'postcss-loader', // postcss loader needed for tailwindcss
-            options: {
-              postcssOptions: {
-                ident: 'postcss',
-                plugins: [tailwindcss, autoprefixer],
-              },
+            {
+                test: /\.css$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {},
+                    },
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            importLoaders: 1,
+                        },
+                    },
+                    {
+                        loader: 'postcss-loader', // postcss loader needed for tailwindcss
+                        options: {
+                            postcssOptions: {
+                                ident: 'postcss',
+                                plugins: [tailwindcss, autoprefixer],
+                            },
+                        },
+                    },
+                ],
             },
-          },
+            {
+                test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
+                type: 'asset/inline',
+            },
         ],
-      },
-      {
-        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
-        type: 'asset/resource',
-      },
-      {
-        test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
-        type: 'asset/inline',
-      },
-    ],
-  },
-  output: {
-    path: path.resolve(__dirname, '..', './build'),
-    filename: 'assets/js/[bundle].[contenthash].js',
-  },
+    },
+    output: {
+        path: path.resolve(__dirname, '..', './build'),
+        filename: 'assets/js/[bundle].[contenthash].js',
+    },
 
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, '..', './public/index.html'),
-      favicon: 'src/assets/images/favicon.svg',
-    }),
-    new MiniCssExtractPlugin({
-      filename: 'assets/css/[bundle].[contenthash].css',
-      chunkFilename: 'assets/css/[id].[contenthash].css',
-    }),
-  ],
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, '..', './public/index.html'),
+            favicon: 'src/assets/images/favicon.svg',
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'assets/css/[bundle].[contenthash].css',
+            chunkFilename: 'assets/css/[id].[contenthash].css',
+        }),
+    ],
 }
